d70d7d6539c6bf41d1272cdb0f4390f5341e96ed
ivanrybkin <rybkin2517@mail.ru>
2023-08-31 21:34:28 +0300

    Initial Commit

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Ivan Rybkin on 31.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemMint
+    }
+
+
+}
+

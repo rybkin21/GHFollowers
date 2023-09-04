53c0b6d93c788a3a6657d5e528610883b833886d
ivanrybkin <rybkin2517@mail.ru>
2023-09-04 19:52:07 +0300

    4. Navigation setup, UITabBarCon & UINavigationCon

---
+++ GHFollowers/FavoritesListVC.swift
@@ -0,0 +1,16 @@
+//
+//  FavoritsVC.swift
+//  GHFollowers
+//
+//  Created by Ivan Rybkin on 04.09.2023.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
---
+++ GHFollowers/SearchVC.swift
@@ -0,0 +1,16 @@
+//
+//  SearchVc.swift
+//  GHFollowers
+//
+//  Created by Ivan Rybkin on 04.09.2023.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+}

b5ce2b813089a96a9c0a7cd5c77ae8d37c826b54
ivanrybkin <rybkin2517@mail.ru>
2023-10-26 18:47:50 +0300

    Result type refactor

--- GHFollowers/Managers/NetworkManager.swift
+++ GHFollowers/Managers/NetworkManager.swift
@@ -8,28 +8,28 @@ class NetworkManager {
 
     private init() {}
 
-    func getFollowers(for username: String, page: Int, completed: @escaping ([Follower]?, String?) -> Void) {
+    func getFollowers(for username: String, page: Int, completed: @escaping (Result<[Follower], GFError>) -> Void) {
         let endpoint = baseUrl + "\(username)/followers?per_page100&page=\(page)"
 
         guard let url = URL(string: endpoint) else {
-            completed(nil, "This username created an invalid request. Please try again.")
+            completed(.failure(.invalidUsername))
             return
         }
 
         let task = URLSession.shared.dataTask(with: url) { data, response, error in
 
             if let _ = error {
-                completed(nil, "Unable to complete your request. Please check your internet connection.")
+                completed(.failure(.unableToComplete))
                 return
             }
 
             guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-                completed(nil, "Invalid response from the server. Please try again.")
+                completed(.failure(.invalidResponse))
                 return
             }
 
             guard let data = data else {
-                completed(nil, "The data received from server was invalid. Please try again.")
+                completed(.failure(.invalidData))
                 return
             }
 
@@ -37,9 +37,9 @@ class NetworkManager {
                 let decoder = JSONDecoder()
                 decoder.keyDecodingStrategy = .convertFromSnakeCase
                 let followers = try decoder.decode([Follower].self, from: data)
-                completed(followers, nil)
+                completed(.success(followers))
             } catch {
-                completed(nil, "The data received from server was invalid. Please try again.")
+                completed(.failure(.invalidData))
             }
         }
         task.resume()
